081b63f1eaaaec0fa2317fe65d88c08969a34bd6
DyadyaSasha <serebryakovalexx@yandex.ru>
2016-12-22 12:29:40 +0300

    Add task changes

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.another, name='another'),
-    url(r'^signup/$', views.another, name='another'),
+    url(r'^login/$', views.test, name='test'),
+    url(r'^signup/$', views.test, name='test'),
     url(r'^question/(\d+)/$', views.test, name='test'),
-    url(r'^ask/', views.another, name='another'),
-    url(r'^popular/$', views.another, name='another'),
-    url(r'^new/$', views.another, name='another'),
+    url(r'^ask/', views.test, name='test'),
+    url(r'^popular/$', views.test, name='test'),
+    url(r'^new/$', views.test, name='test'),
+    url(r'^$', views.test, name='test'),
 ]
